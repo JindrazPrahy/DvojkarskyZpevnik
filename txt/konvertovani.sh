b5c91bc4fab98a73900fd10b95fe2d57b76d63b3
Sordid Simon <jd@mamlasinky.com>
2018-01-28 16:58:07 +0100

    Restrukturizace podruhé
    Změnil jsem jména adresářů, aby se nejmenovali s diakritikou.
    Oddělil jsem složku na pdf a txt formát.
    Vymazal jsem duplicitní soubory a složky od minulého commitu, kdy jsem je tam omylem nechal.

---
+++ txt/konvertovani.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/sh
+#Konvertuj všechny pdfka do txt souborů timhle řádkem:
+
+for f in ../pdf/*.pdf;do TEMP="${f%%.*}" && less $TEMP.pdf &> txt/$TEMP.txt;done && sed -i '$d' *.txt
+
+#ten for loop to konvertuje, ten sed maže poslední řádek souboru, jelikož ten
+#se nějak zaplní něčím, co nechceme
+
