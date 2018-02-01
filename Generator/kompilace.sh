9cb7cf7c16e35d8944d694c8e021712708c0d7f1
JindrazPrahy <jd@mamlasinky.com>
2018-02-01 16:17:55 +0100

    Úprava kompilačního skriptu

--- Generator/kompilace.sh
+++ Generator/kompilace.sh
@@ -3,13 +3,24 @@
 # Možnost, že se kompilátor zavolá s jedním konkrétním songem
 # např.: ./kompilace.sh -f ../songy/Amerika.tex
 # Další optiony jde přidat pomocí ... elif test; then ... else ... fi nebo použít switch
-if [ "$1" -eq "-f" ]; then
-    if [ "$#" -ne "2" ]; then
+echo "$1"
+echo "$2"
+if [ "$1" = "-f" ]; then   #Pozn. -eq porovnává integery, = porovnává stringy
+    if [ "$#" != "2" ]; then #Podobně -ne a !=
         echo "Missing file arg."
     else    
-        cat generator.tex | sed s/\\input{..\/songy\/.*.tex}/\\input{$2}/ > /tmp/generator.tex
-        pdflatex -synctex=1 -interaction=batchmode /tmp/generator.tex
-        mv generator.pdf ../pdf/$2.pdf
+        #cat generator.tex | sed s/\\input{..\/songy\/.*.tex}/\\input{$2}/ > /tmp/generator.tex
+        #pdflatex -synctex=1 -interaction=batchmode /tmp/generator.tex
+	#Změna kódu, recykloval jsem to, co už bylo jednou použito
+	eachfile=$2
+	head -n -1 generator.tex > temp.txt ; mv temp.txt generator.tex 
+	head -n -1 generator.tex > temp.txt ; mv temp.txt generator.tex #Odstranění posl. dvou řádků
+	echo "\input{${eachfile}}" >> generator.tex  
+	echo "\end{document}" >> generator.tex  #Úprava tex souboru pro kompilaci individuálních písní
+	pdflatex -synctex=1 -interaction=batchmode generator.tex #kompilace
+	eachfile="${eachfile#../songy/}"
+	eachfile="${eachfile%.tex}" #Dostání pouze jména souboru bez přípony
+	mv generator.pdf ../pdf/${eachfile}.pdf #Přesun hotového pdf do nové složky
     fi 
 else
     yourfilenames=`ls ../songy/*.tex`
