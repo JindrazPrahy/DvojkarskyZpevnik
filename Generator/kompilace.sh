a3bc031c2db613b257802eec35dfadb578606ed0
JindrazPrahy <jd@mamlasinky.com>
2018-02-01 16:46:40 +0100

    Ještě lepší vylepšení kompilace.sh

--- Generator/kompilace.sh
+++ Generator/kompilace.sh
@@ -1,26 +1,28 @@
 #!/bin/sh
 
 # Možnost, že se kompilátor zavolá s jedním konkrétním songem
-# např.: ./kompilace.sh -f ../songy/Amerika.tex
+# např.: ./kompilace.sh -f Amerika
 # Další optiony jde přidat pomocí ... elif test; then ... else ... fi nebo použít switch
-echo "$1"
-echo "$2"
 if [ "$1" = "-f" ]; then   #Pozn. -eq porovnává integery, = porovnává stringy
     if [ "$#" != "2" ]; then #Podobně -ne a !=
         echo "Missing file arg."
     else    
+	echo "Kompilace písně $2:"
+	eachfile=$2
+    	eachfile="${eachfile%.tex}" #Dostání pouze jména souboru bez přípony
+	eachfile="${eachfile#../songy/}"
+	#Když je tohle na začátku, nemusí $2 být plná cesta k písničce ve složce songy
+	#Stačí třeba Amerika
         #cat generator.tex | sed s/\\input{..\/songy\/.*.tex}/\\input{$2}/ > /tmp/generator.tex
         #pdflatex -synctex=1 -interaction=batchmode /tmp/generator.tex
 	#Změna kódu, recykloval jsem to, co už bylo jednou použito
-	eachfile=$2
 	head -n -1 generator.tex > temp.txt ; mv temp.txt generator.tex 
 	head -n -1 generator.tex > temp.txt ; mv temp.txt generator.tex #Odstranění posl. dvou řádků
-	echo "\input{${eachfile}}" >> generator.tex  
+	echo "\input{../songy/${eachfile}.tex}" >> generator.tex  
 	echo "\end{document}" >> generator.tex  #Úprava tex souboru pro kompilaci individuálních písní
 	pdflatex -synctex=1 -interaction=batchmode generator.tex #kompilace
-	eachfile="${eachfile#../songy/}"
-	eachfile="${eachfile%.tex}" #Dostání pouze jména souboru bez přípony
 	mv generator.pdf ../pdf/${eachfile}.pdf #Přesun hotového pdf do nové složky
+	echo "Alles gute"
     fi 
 else
     yourfilenames=`ls ../songy/*.tex`
@@ -36,4 +38,5 @@ else
        eachfile="${eachfile%.tex}" #Dostání pouze jména souboru bez přípony
        mv generator.pdf ../pdf/${eachfile}.pdf #Přesun hotového pdf do nové složky
     done
+    echo "Alles gute"
 fi
